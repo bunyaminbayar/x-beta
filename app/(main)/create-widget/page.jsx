a79a94830462a63495d4a49525a5d5a8a4a56149
bunyamin151 <104002838+bunyaminGCL@users.noreply.github.com>
2023-03-19 21:06:01 +0100

    before add menu in create widget

---
+++ app/(main)/create-widget/page.jsx
@@ -0,0 +1,307 @@
+"use client"; // this is a client component 
+
+import { useState } from 'react';
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import MenuItem from '@mui/material/MenuItem';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MUISwitch from './../../components/switch'
+
+
+function CreateWidget() {
+
+    // States
+    const [widgetName, setWidgetName] = useState('');
+    const [widgetNameError, setWidgetError] = useState('');
+    const [widgetNameReady, setWidgetNameReady] = useState(false);
+    // widget title
+    const [widgetTitle, setwidgetTitle] = useState('');
+    const [widgetTitleError, setwidgetTitleError] = useState('');
+    const [widgetTitleReady, setWidgetTitleReady] = useState(false);
+    // cta text
+    const [ctaText, setCtaText] = useState('');
+    const [ctaTextError, setCtaTextError] = useState('');
+    const [ctaTextReady, setCtaTextReady] = useState(false);
+    //
+    // Cta Link
+    const [ctaLink, setCtaLink] = useState('');
+    const [ctaLinkError, setCtaLinkError] = useState('');
+    const [ctaLinkReady, setCtaLinkReady] = useState(false);
+    // Widget position
+    const [widgetPosition, setwidgetPosition] = useState('Top Center');
+    //
+    const [widgetBody, setwidgetBody] = useState('');
+    // 
+    const [icon, setIcon] = useState('demo');
+
+    //
+    const handleChangeWidgetName = (event) => {
+        if (event.target.value.length < 1) {
+            setWidgetError('Please enter Widget Name');
+            setWidgetNameReady(false);
+        } else {
+            setWidgetName(event.target.value);
+            setWidgetError('');
+            setWidgetNameReady(true);
+        }
+    };
+
+    // widget title
+    const handleChangeWidgetTitle = (event) => {
+        if (event.target.value.length < 1) {
+            setwidgetTitleError('Please enter Title');
+            setWidgetTitleReady(false);
+        } else {
+            setwidgetTitle(event.target.value);
+            setwidgetTitleError('');
+            setWidgetTitleReady(true);
+        }
+    };
+
+    // Cta text
+    const handleChangeCtaText = (event) => {
+        if (event.target.value.length < 1) {
+            setCtaTextError('Please enter CTA Text');
+            setCtaTextReady(false);
+        } else {
+            setCtaText(event.target.value);
+            setCtaTextError('');
+            setCtaTextReady(true);
+        }
+    };
+
+    // cta link
+    const handleChangeCtaLink = (event) => {
+        if (event.target.value.length < 1) {
+            setCtaLinkError('Please enter CTA Link');
+            setCtaLinkReady(false);
+        } else {
+            setCtaLink(event.target.value);
+            setCtaLinkError('');
+            setCtaLinkReady(true);
+        }
+    };
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        //
+        if (widgetNameReady === true && widgetTitleReady == true && ctaTextReady === true && ctaLinkReady === true) {
+            // success form
+            console.log(widgetName, widgetTitle, ctaText, ctaLink, widgetPosition, widgetBody, "success");
+        } if (widgetNameReady === false) {
+            setWidgetError('Please enter Widget Name');
+        } if (widgetTitleReady === false) {
+            setwidgetTitleError('Please enter Title');
+        } if (ctaTextReady === false) {
+            setCtaTextError('Please enter CTA Text');
+        } if (ctaLinkReady === false) {
+            setCtaLinkError('Please enter CTA Link');
+        }
+
+    };
+
+    // MUI Meida query rulles
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    //
+
+    return (
+        <>
+            <Box padding={isSmallScreen ? '20px 20px 20px 20px' : '20px 56px 20px 56px'}>
+                <Breadcrumbs maxItems={4} aria-label="breadcrumb" fontSize={'14px'} lineHeight={'18px'}>
+                    <Link underline="hover" href="#" color={'#0F62FE'}>
+                        ACME LTD
+                    </Link>
+                    <Link underline="hover" href="home" color={'#0F62FE'}>
+                        Home
+                    </Link>
+                    <Typography color="#161616">Create Widget</Typography>
+                </Breadcrumbs>
+            </Box>
+            <Box display="flex" height="calc(100% - 70px)" justifyContent="center" alignItems="center" padding={isSmallScreen ? '0px 20px 0px 20px' : '0px 0px 0px 0px'}>
+                <Box width={'100%'} maxWidth='945px' color="#161616">
+                    <Box sx={{
+                        '--Grid-borderWidth': '1px',
+                        border: 'var(--Grid-borderWidth) solid',
+                        borderColor: 'divider',
+                        p: '37px 28px 28px 28px',
+                        borderRadius: '12px',
+                        mb: 4,
+                    }}>
+                        <Grid container display="flex" alignItems="center">
+                            <Grid item xs={12} sm={6} paddingBottom={isSmallScreen ? '16px' : '0px'}>
+                                <Typography sx={{ mb: 3 }} component="h1" variant="h5" fontWeight="medium" fontSize={'18px'} lineHeight={'21px'}>
+                                    Create Widget
+                                </Typography>
+                                <Box className='createWidgetFrom' component="form" onSubmit={submitForm} noValidate>
+                                    <Typography color="#525252" fontSize="12px" lineHeight="16px" sx={{ mt: 4, mb: 1 }}>
+                                        Widget Name
+                                    </Typography>
+                                    <TextField
+                                        onChange={handleChangeWidgetName}
+                                        error={!!widgetNameError}
+                                        helperText={widgetNameError}
+                                        fullWidth
+                                        id="filled-basic"
+                                        variant="filled"
+                                        placeholder='H-birthday-2023-promo'
+                                    />
+                                    <ButtonGroup variant="outlined" size="small" aria-label="small button group" sx={{ mt: 2, mb: 2 }}>
+                                        <Button sx={{ padding: '6px 32px 6px 16px', fontSize: '14px', lineHeight: '18px', borderColor: '#161616', color: '#161616', textTransform: 'inherit', '&:hover': { borderColor: '#161616', } }}>Icon + text</Button>
+                                        <Button sx={{ padding: '6px 32px 6px 16px', fontSize: '14px', lineHeight: '18px', borderColor: '#161616', color: '#161616', textTransform: 'inherit', '&:hover': { borderColor: '#161616', } }}>Icon only</Button>
+                                    </ButtonGroup>
+                                    <Box sx={{
+                                        '--Grid-borderWidth': '1px',
+                                        border: 'var(--Grid-borderWidth) solid',
+                                        borderColor: 'divider',
+                                        p: '16px 16px 24px 16px',
+                                        borderRadius: '12px',
+                                        boxShadow: '0px 4px 7px rgba(0, 0, 0, 0.05)'
+                                    }}>
+                                        <Typography fontSize="13px" variant='h5' component='h5' lineHeight="16px" color='#161616' fontWeight='500' sx={{ mb: '20px' }}>
+                                            Widget configuration
+                                        </Typography>
+                                        <Grid container display="flex" alignItems="center" sx={{ alignItems: 'baseline' }}>
+                                            <Grid item xs={3} >
+                                                <Typography color="#525252" fontSize="12px" lineHeight="16px" sx={{ mb: 1 }}>
+                                                    Icon
+                                                </Typography>
+                                                <TextField
+                                                    className='iconInput'
+                                                    id="filled-select-currency"
+                                                    select
+                                                    fontSize='14px'
+                                                    fullWidth
+                                                    variant="filled"
+                                                    onChange={(event) => setIcon(event.target.value)}
+                                                    SelectProps={{
+                                                        IconComponent: ExpandMoreIcon,
+                                                    }}
+                                                >
+                                                    <MenuItem key='0' value='1'>
+                                                        menu
+                                                    </MenuItem>
+                                                </TextField>
+                                            </Grid>
+                                            <Grid item xs={9} sx={{ pl: 2 }}>
+                                                <Typography color="#525252" fontSize="12px" lineHeight="16px" sx={{ mb: 1 }}>
+                                                    Title
+                                                </Typography>
+                                                <TextField
+                                                    className='widggetTitleError'
+                                                    onChange={handleChangeWidgetTitle}
+                                                    error={!!widgetTitleError}
+                                                    helperText={widgetTitleError}
+                                                    fullWidth
+                                                    id="filled-basic2"
+                                                    variant="filled"
+                                                    placeholder='Happy Birthday!'
+                                                />
+                                            </Grid>
+                                        </Grid>
+                                        <Typography color="#525252" fontSize="12px" lineHeight="16px" sx={{ mb: 1, mt: 2 }}>
+                                            Body
+                                        </Typography>
+                                        <TextField
+                                            fullWidth
+                                            className='createWidgetTextArea'
+                                            id="outlined-multiline-static"
+                                            onChange={(event) => setwidgetBody(event.target.value)}
+                                            multiline
+                                            rows={3}
+                                            defaultValue="Default Value"
+                                            variant="filled"
+                                        />
+                                        <Typography color="#525252" fontSize="12px" lineHeight="16px" sx={{ mb: 1, mt: 2 }}>
+                                            Add CTA button
+                                        </Typography>
+                                        <MUISwitch />
+                                        <Grid container display="flex" alignItems="center" sx={{ mt: 2, alignItems: 'baseline'}}>
+                                            <Grid item xs={4} >
+                                                <Typography color="#525252" fontSize="12px" lineHeight="16px" sx={{ mb: 1 }}>
+                                                    CTA Text
+                                                </Typography>
+                                                <TextField
+                                                    className='ctaTextInput'
+                                                    onChange={handleChangeCtaText}
+                                                    error={!!ctaTextError}
+                                                    helperText={ctaTextError}
+                                                    fullWidth
+                                                    id="filled-basic2"
+                                                    variant="filled"
+                                                    placeholder='Cta Text'
+                                                />
+
+                                            </Grid>
+                                            <Grid item xs={8} sx={{ pl: 2 }}>
+                                                <Typography color="#525252" fontSize="12px" lineHeight="16px" sx={{ mb: 1 }}>
+                                                    CTA Text
+                                                </Typography>
+                                                <TextField
+                                                    className='widggetTitleError'
+                                                    onChange={handleChangeCtaLink}
+                                                    error={!!ctaLinkError}
+                                                    helperText={ctaLinkError}
+                                                    fullWidth
+                                                    id="filled-basic2"
+                                                    variant="filled"
+                                                    placeholder='/promotions'
+                                                />
+                                            </Grid>
+                                        </Grid>
+                                        <Typography color="#525252" fontSize="12px" lineHeight="16px" sx={{ mb: 1, mt: '20px' }}>
+                                            Widget Position
+                                        </Typography>
+                                        <TextField
+                                            id="filled-select-currency"
+                                            select
+                                            fullWidth
+                                            defaultValue="top-center"
+                                            variant="filled"
+                                            onChange={(event) => setwidgetPosition(event.target.value)}
+                                            SelectProps={{
+                                                IconComponent: ExpandMoreIcon,
+                                            }}
+                                        >
+                                            <MenuItem value='top-center'>Top Center</MenuItem>
+                                            <MenuItem value='center-center'>Center Center</MenuItem>
+                                            <MenuItem value='top-left'>Top Left</MenuItem>
+                                            <MenuItem value='top-right'>Top Right</MenuItem>
+                                        </TextField>
+                                    </Box>
+                                    <Button
+                                            type="submit"
+                                            size="large"
+                                            variant="contained"
+                                            fontWeight="medium"
+                                            sx={{ fontSize: '14px', lineHeight: '16px', mt: 3, pt: 2, pr: 8, pb: 2, pl: 2, borderRadius: 0, textTransform: 'inherit', background: '#0F62FE' }}
+                                        >
+                                            Create widget
+                                        </Button>
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12} sm={6} paddingBottom={isSmallScreen ? '16px' : '0px'}>
+                                <Typography sx={{ mb: '40px' }} component="h3" variant="h3" fontWeight="medium" fontSize={isSmallScreen ? '20px' : '24px'} lineHeight={isSmallScreen ? '22px' : '18px'}>
+                                    666
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Box>
+
+
+                </Box>
+            </Box>
+        </>
+    );
+}
+
+export default CreateWidget;
---
+++ app/components/switch/index.js
@@ -0,0 +1,104 @@
+"use client"; // this is a client componentimport React from 'react';
+import * as React from 'react';
+import clsx from 'clsx';
+import { styled } from '@mui/system';
+import useSwitch from '@mui/base/useSwitch';
+
+const grey = {
+    400: '#BFC7CF',
+    800: '#2F3A45',
+};
+
+const SwitchRoot = styled('span')`
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 36px;
+  padding: 8px;
+`;
+
+const SwitchInput = styled('input')`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  z-index: 1;
+  margin: 0;
+  cursor: pointer;
+`;
+
+const SwitchThumb = styled('span')`
+  position: absolute;
+  display: block;
+  width: 30px;
+  height: 30px;
+  border-radius: 8px;
+  top: 3px;
+  left: 4px;
+  transition: transform 150ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &::before {
+    display: block;
+    content: '';
+
+    width: 100%;
+    height: 100%;
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="CloseIcon"><path fill="${encodeURIComponent(
+    '#e74c3c',
+)}" d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>')
+      center center no-repeat;
+      background-color: #fff;
+    border-radius: 50%;
+  }
+
+  &.focusVisible {
+    background-color: #79b;
+  }
+
+  &.checked {
+    transform: translateX(24px);
+
+    &::before {
+        background-color: #fff;
+        border-radius: 50%;
+      background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="CheckIcon"><path fill="${encodeURIComponent(
+    '#198038',
+)}" d="M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"></path></svg>');
+    }
+  }
+`;
+
+const SwitchTrack = styled('span')(
+    ({ theme }) => `
+  width: 100%;
+  height: 100%;
+  display: block;
+`,
+);
+
+function MUISwitch(props) {
+    const { getInputProps, checked, disabled, focusVisible } = useSwitch(props);
+
+    const stateClasses = {
+        checked,
+        disabled,
+        focusVisible,
+    };
+
+    return (
+        <diV className='addCtaSwitch'>
+            <SwitchRoot className={clsx(stateClasses)}>
+                <SwitchTrack>
+                    <SwitchThumb className={clsx(stateClasses)} />
+                </SwitchTrack>
+                <SwitchInput {...getInputProps()} aria-label="Demo switch" />
+            </SwitchRoot>
+        </diV>
+    );
+}
+
+export default function UseSwitchesCustom() {
+    return <MUISwitch defaultChecked />;
+}
